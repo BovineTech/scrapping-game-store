9a9051d3bfe864c119c86935e19d3feaafed31c5
spearhead1024 <thierrycaillibot5@gmail.com>
2024-12-23 03:54:38 -0800

    create-reactproject

---
+++ display-gameinfo-mearn/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RoutesIndex from "./routes/Index";
+
+const App = () => {
+  return (
+    <>
+      <h1>Start React Project</h1>
+    </>
+  );
+};
+
+export default App;

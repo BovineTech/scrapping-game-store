c7e3b05c9dd456505a6a8fb39b0a46d3d3135e0f
spearhead1024 <thierrycaillibot5@gmail.com>
2025-01-08 06:16:14 -0800

    completed-logging

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,7 @@
 import schedule
 import time
 import subprocess
+import logging
 from utils import update_mongo, get_mongo_db
 
 SCRAPERS = [
@@ -10,16 +11,32 @@ SCRAPERS = [
     "scraper_xbox.py",
 ]
 
+# Configure logging
+logging.basicConfig(
+    filename="scraper.log",
+    filemode="a",
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+def log_info(message):
+    logging.info(message)
+    print(message)
+
+def log_error(message):
+    logging.error(message)
+    print(message)
+
 def run_scrapers():
     for scraper in SCRAPERS:
         try:
-            print(f"========== Starting {scraper}... ==========")
+            log_info(f"========== Starting {scraper}... ==========")
             subprocess.run(["python", scraper], check=True)
-            print(f"========== Successfully ran {scraper}. ==========")
             db = get_mongo_db()
             update_mongo(db, scraper)
+            log_info(f"========== Successfully ran {scraper} and Updated db. ==========")
         except Exception as e:
-            print(f"Error running {scraper}: {e}")
+            log_error(f"Error running {scraper}: {e}")
 
 def main():
     schedule.every(1).seconds.do(run_scrapers)

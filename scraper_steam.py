8f40847aa7e5246223b3eb57fcfa4422726231ca
spearhead1024 <thierrycaillibot5@gmail.com>
2025-01-16 13:49:01 -0800

    use proxies

--- scraper_steam.py
+++ scraper_steam.py
@@ -1,6 +1,6 @@
 import requests
 import time
-from utils import save_to_mongo, get_mongo_db, log_info, regions_steam
+from utils import save_to_mongo, get_mongo_db, log_info, regions_steam, my_proxies
 
 STEAM_API_URL = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
 
@@ -9,10 +9,10 @@ def fetch_steam_apps():
     response.raise_for_status()
     return response.json()["applist"]["apps"]
 
-def fetch_game_details(app_id):
+def fetch_game_details(app_id, proxy):
     base_url = "https://store.steampowered.com/api/appdetails"
     try:
-        response = requests.get(base_url, params={"appids": app_id, "l": "en"})
+        response = requests.get(base_url, params={"appids": app_id, "l": "en"}, proxies=proxy, timeout=10)
         response.raise_for_status()
         data = response.json()
         
@@ -26,7 +26,7 @@ def fetch_game_details(app_id):
         while index < len(regions_steam):
             region = regions_steam[index]
             try:
-                response = requests.get(base_url, params={"appids": app_id, "cc": region, "l": "en"})
+                response = requests.get(base_url, params={"appids": app_id, "cc": region, "l": "en"}, proxies=my_proxies[index % 10], timeout=10)
                 response.raise_for_status()
                 data = response.json()
                 if str(app_id) in data and data[str(app_id)]["success"]:
@@ -36,9 +36,8 @@ def fetch_game_details(app_id):
                     prices[region] = "Not Available"
                 index += 1
             except Exception as e:
-                time.sleep(60)
-                print("Steam : ", app_id, "------too many requests : waiting for server")
-                
+                print("Steam : ", app_id, f"------too many requests : {my_proxies[index % 10]} : waiting for server")
+                print(f"Error fetching game details: {e}")
     except Exception as e:
         return {"error": f"Error fetching game details: {e}"}
 
@@ -64,13 +63,14 @@ def main():
     index = 0
     while index < len(apps):
         app = apps[index]
-        game_data = fetch_game_details(app["appid"])
+        proxy = my_proxies[index % 10]
+        game_data = fetch_game_details(app["appid"], proxy)
         if "error" in game_data:
             if "429" in game_data['error']:
-                time.sleep(60)
-                # print(app['appid'], "------too many requests : waiting for server")
+                # time.sleep(60)
+                print(app['appid'], f"------too many requests : {proxy} : waiting for server")
                 continue
-            # else: print("Steam : ", app['appid'], "------",game_data["error"])
+            else: print("Steam : ", app['appid'], "------",game_data["error"])
         else:
             save_to_mongo(db, "steam_games", game_data)
             log_info(f"Saved Steam game {index+1}: {game_data['title']}")

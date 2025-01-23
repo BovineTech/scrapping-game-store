b270b6a16e5ce8834bb38a8e1392d819bb4e470b
spearhead1024 <thierrycaillibot5@gmail.com>
2025-01-22 21:19:41 -0800

    api test

--- api_server.py
+++ api_server.py
@@ -392,6 +392,7 @@ def get_game_count():
     else:
         return jsonify({"msg": "Invalid service"}), 400
     
+    print(collection)
     count = collection.count_documents({})
     return jsonify({"count": count}), 200
 
